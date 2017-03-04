c09397431cea7794bb99ef28fc01b18e4d3ec815
JediWattson <rdaly@hartford.edu>
2017-03-03 19:20:47 -0500

    Add files via upload

---
+++ primegen.py
@@ -0,0 +1,51 @@
+import random
+from math import log
+def randprime(bits):
+	s = '1'
+	b = 0
+	while b < bits-1:
+		t = random.SystemRandom().randrange(2)
+		if t == 1:
+			s = s + '1'
+		else:
+			s = s + '0'
+		b = b + 1
+	s = s + '1'
+	prime = int(s, 2)
+
+	while True:
+		v = 1
+		n = prime - 1
+		s = 0
+		while n%2 ==0:
+			n = n/2
+			s = s + 1
+		pot = 100.00
+		a = random.SystemRandom().randrange(2, int(pot))
+		for z in range(2, int(pot)):
+			prtest = False
+			for w in range(s):
+				a = z
+				binp = bin((2**w)*n)
+				sig = 1	
+				if z > 10:
+					print float(z/pot)*100
+				for x in range(len(binp)-2):
+					a = (a*a)%prime
+					if binp[len(binp) - (x+1)] is '1':
+						sig = sig * (a)
+				if (sig%prime) == 1 or prime-(sig%prime) == 1:
+					prtest = True
+					break
+
+			if not prtest:
+				v = 0
+				break
+	
+		if v == 1: 
+			return prime
+			break
+		if prime%2 == 0:
+			prime = prime + 1
+		else:
+			prime = prime + 2
---
+++ rsa.py
@@ -0,0 +1,66 @@
+import random
+import time
+from math import sqrt
+from primegen import randprime
+
+def crypt(message):
+	i = 0
+	dec = ''
+	if len(message)%2 == 1:
+		end = len(message)
+	else:
+		end = len(message)  
+
+	while i < len(message) - 1:
+		h = str(message[i]+ message[i+1])
+		i = i + 2
+		dec = dec + chr(int(h, 16))
+	print dec
+
+
+start = time.time()
+
+randprime1 = randprime(1024)
+randprime2 = randprime(1024)
+tot = (randprime2 - 1) * (randprime1 - 1)
+n = randprime1 * randprime2
+
+phi = [1 for x in range(2**16)]
+e = 3
+while True:
+	if phi[e] == 1 and tot%e != 0:
+		break
+	elif phi[e] == 1:
+		z = e
+		while z < len(phi)-1:
+			phi[z] = 0
+			z = z + e
+	e = e + 2
+
+k = 1
+while True:
+	if ((k*tot) + 1)%e == 0:
+		break
+	k = k + 1
+
+d = ((k*tot) + 1)/e
+
+s = "hello world how's it going?"
+hi = ''.join(hex(ord(x))[2:] for x in s)
+hi = int(hi, 16)
+c = (hi**e)%n
+
+bd = bin(d)
+sigma = 1
+for x in range(len(bd)-2):
+	if bd[len(bd) - (x+1)] is '1':
+		sigma = sigma * (c%n)
+	c = (c*c)%n
+
+end = time.time()
+print end - start
+u = sigma%n
+print len(str(n))
+d = hex(u)[2:len(hex(u))]
+
+crypt(d)
