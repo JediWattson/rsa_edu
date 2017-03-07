6371f5cc28654e222cfa8018928c3e31c62c8692
Robert Daly <rdaly@hartford.edu>
2017-03-07 15:52:33 -0500

    better variable names

--- rsa.py
+++ rsa.py
@@ -11,24 +11,25 @@ class rsa_edu:
                 self.n = randprime1 * randprime2
 
                 phi = [1 for x in range(2**16)]
-                self.e = 3
+                self.encrypt_key = 3
                 while True:
-                        if phi[self.e] == 1 and tot%self.e != 0:
+                        if phi[self.encrypt_key] == 1 and tot%self.encrypt_key != 0:
                                 break
-                        elif phi[self.e] == 1:
-                                z = self.e
+                        elif phi[self.encrypt_key] == 1:
+                                z = self.encrypt_key
                                 while z < len(phi)-1:
                                         phi[z] = 0
-                                        z = z + self.e
-                        self.e = self.e + 2
+                                        z = z + self.encrypt_key
+                        self.encrypt_key = self.encrypt_key + 2
                 self.k = 1
                 while True:
-                        if ((self.k*tot) + 1)%self.e == 0:
+                        if ((self.k*tot) + 1)%self.encrypt_key == 0:
                                 break
                         self.k = self.k + 1
-                self.decrypt_key = ((self.k*tot) + 1)/self.e
+                self.decrypt_key = ((self.k*tot) + 1)/self.encrypt_key
                 self.encrypt()
-
+                self.decrypt()
+				
         def hextostring(self, message):
                 i = 0
                 self.dec = ''
@@ -36,16 +37,15 @@ class rsa_edu:
                         h = str(message[i]+ message[i+1])
                         i = i + 2
                         self.dec = self.dec + chr(int(h, 16))
-				
-						
+					
         def encrypt(self):
-				be = bin(self.encrypt)
+				be = bin(self.encrypt_key)
 				sigma = 1
 				for x in range(len(be) - 2):
 						if be[-(x+1)] is '1':
 								sigma = sigma*(self.hi%self.n)
 						self.hi = (self.hi*self.hi)%self.n
-                self.crypt = (self.hi**self.e)%self.n
+				self.crypt = sigma%self.n
                 
         def decrypt(self):
                 bd = bin(self.decrypt_key)
--- rsaDriver.py
+++ rsaDriver.py
@@ -1,9 +1,7 @@
 from rsa import rsa_edu
 import time
 
-for x in range(300, 2000):
+for x in range(600, 2000):
 	start = time.time()
 	r = rsa_edu("hello world@!!!!!!!!", x)
-	r.decrypt()
-	print  
-	print r.dec, '\t' + str(x), '\t' + str(- (start - time.time()))
+	print r.dec, '\t' + str(len(bin(r.n))), '\t' + str(- (start - time.time()))
